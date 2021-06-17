c2020185f58bce62e53c807d341c497bf8eb937e
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2021-06-17 11:20:37 +0530

    build: configure lint and workflow for lint

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello World from TNB List
-    </div>
+    <>
+      <div className="App">Hello World from TNB List</div>
+    </>
   );
 }
 
